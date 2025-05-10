5ecdb6eeffff23be69975a42956fb354c6f189fa
Luke Barrier <lukebbarrier06@outlook.com>
2025-05-09 20:12:34 -0400

    fix: types

--- backend/app/models/business.py
+++ backend/app/models/business.py
@@ -1,5 +1,6 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .db import db, environment, SCHEMA
 from datetime import datetime
+
 
 class Business(db.Model):
     __tablename__ = "businesses"
@@ -8,41 +9,44 @@ class Business(db.Model):
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     country = db.Column(db.String(50))
     address = db.Column(db.String(100), nullable=False)
     city = db.Column(db.String(50), nullable=False)
     type = db.Column(db.String(50), nullable=False)
     state = db.Column(db.String(50), nullable=False)
-    zipcode = db.Column(db.Integer(5), nullable=False)
+    zipcode = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(500))
-    lat = db.Column(db.Number(50), nullable=True)
-    lng = db.Column(db.Number(50), nullable=True)
-    price_range = db.Column(db.Integer(1),minimum=1, maximum=4, nullable=False)
-    created_at = db.Column(db.Datetime, default=datetime.utcnow)
-    updated_at = db.Column(db.Datetime)
+    descriphtion = db.Column(db.String(500))
+    lat = db.Column(db.Float, nullable=True)
+    lng = db.Column(db.Float, nullable=True)
+    price_range = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
     # Relationship
-    user = db.relationship('User', back_populates='businesses')
-    reviews = db.relationship('Review', back_populates='business')
-    images = db.relationship('Images', back_populates='business')
+    user = db.relationship("User", back_populates="businesses")
+    reviews = db.relationship("Review", back_populates="business")
+    images = db.relationship("Image", back_populates="business")
 
     def to_dict(self):
         return {
-            "id": self.id, 
-            "owner_id": self.owner_id, 
+            "id": self.id,
+            "owner_id": self.owner_id,
             "country": self.country,
-            "address": self.address, 
-            "city": self.city, 
+            "address": self.address,
+            "city": self.city,
             "type": self.type,
-            "state": self.state, 
-            "zipcode": self.zipcode, 
-            "name": self.name, 
+            "state": self.state,
+            "zipcode": self.zipcode,
+            "name": self.name,
             "description": self.description,
-            "lat": self.lat, 
+            "lat": self.lat,
             "lng": self.lng,
-            "price_range": self.price_range, 
-            "created_at": self.created_at.isoformat(), 
-            "updated_at": self.updated_at.isoformat()
+            "price_range": self.price_range,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
         }
+
