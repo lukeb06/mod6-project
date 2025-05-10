e3b5369ee19f3883c29b0989ce3b5665e827d1e6
Luke Barrier <lukebbarrier06@outlook.com>
2025-05-10 12:09:47 -0400

    added: create business route

--- backend/app/api/business_routes.py
+++ backend/app/api/business_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Business
+from flask_login import login_required, current_user
+from app.models.db import db
 
 business_routes = Blueprint("business", __name__)
 
@@ -21,4 +23,31 @@ def business(id: int):
     """
 
     business = Business.query.get(id)
+    return jsonify(business.to_dict())
+
+
+@business_routes.route("/", methods=["POST"])
+@login_required
+def create_business():
+    """
+    Create a business
+    """
+
+    data = request.get_json()
+    business = Business(
+        name=data.get("name"),
+        country=data.get("country"),
+        address=data.get("address"),
+        city=data.get("city"),
+        type=data.get("type"),
+        state=data.get("state"),
+        zipcode=data.get("zipcode"),
+        price_range=data.get("price_range"),
+        description=data.get("description"),
+        lat=data.get("lat"),
+        lng=data.get("lng"),
+        owner_id=current_user.id,
+    )
+    db.session.add(business)
+    db.session.commit()
     return jsonify(business.to_dict())
